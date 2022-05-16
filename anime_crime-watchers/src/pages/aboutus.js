27f3d762a766c90f72510c7d350b49d14e3fcbb0
tanyaleepr <tanyaleepr@gmail.com>
2022-05-16 14:56:38 -0500

    moved files to src

--- anime_crime-watchers/src/pages/aboutus.js
+++ anime_crime-watchers/src/pages/aboutus.js
@@ -1,26 +1,23 @@
 import React from 'react';
-
+import coverImage from '../../assets/';
 function About() {
-	return (
-		<section>
-			<div className="center" id="about">
-				<h1 className="page-header">About us</h1>
-			</div>
-			<div className="center">
-				<img
-					src={require('../../assets/images/')}
-					alt="about-me"
-					className="photo"
-				/>
-			</div>
-			<div>
-				<p>
-				Hi, lorem.
-				</p>
-				
-			</div>
-		</section>
-	);
+  return (
+    <section className="my-5">
+      <h1 id="about">Who am I?</h1>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
+          condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
+          mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum.
+          Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique
+          justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea
+          dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh.
+          Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default About;
--- anime_crime-watchers/src/pages/contactus.js
+++ anime_crime-watchers/src/pages/contactus.js
@@ -1,97 +1,66 @@
-import React, { useState } from "react";
-import { validateEmail } from "../../utils/helpers";
+import React, { useState } from 'react';
 
-function Contact() {
-	const [formState, setFormState] = useState({
-		name: "",
-		email: "",
-		message: "",
-	});
+import { validateEmail } from '../../utils/helpers';
 
-	const [errorMessage, setErrorMessage] = useState("");
+function ContactForm() {
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
-	const { name, email, message } = formState;
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
 
-	function handleChange(e) {
-		if (e.target.name === "email") {
-			const isValid = validateEmail(e.target.value);
-			if (!isValid) {
-				setErrorMessage("Your email is invalid.");
-			} else {
-				if (!e.target.value.length) {
-					setErrorMessage(`${e.target.name} is required.`);
-				} else {
-					setErrorMessage("");
-				}
-			}
-		}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
+    }
+  };
 
-		if (!errorMessage) {
-			setFormState({ ...formState, [e.target.name]: e.target.value });
-		}
-	}
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log('Handle Form', formState);
+    }
+  };
 
-	function handleBlank(e) {
-		if (e.target.name === "Name" || e.target.name === "Message") {
-			if (!e.target.value.length) {
-				setErrorMessage(`${e.target.name} is required.`);
-			} else {
-				setErrorMessage("");
-			}
-		}
-
-		if (!errorMessage) {
-			setFormState({ ...formState, [e.target.name]: e.target.value });
-		}
-	}
-
-	return (
-		<section>
-			<div className="center">
-				<h2 className="page-header">Contact Me</h2>
-			</div>
-			<div>
-				<form id="contact-form">
-					<div>
-						<label htmlFor="Name">Name:</label>
-						<br></br>
-						<input
-							type="text"
-							defaultValue={name}
-							onBlur={handleBlank}
-							name="Name"
-						/>
-					</div>
-					<div>
-						<label htmlFor="email">Email address:</label>
-						<br></br>
-						<input
-							type="email"
-							defaultValue={email}
-							name="email"
-							onBlur={handleChange}
-						/>
-					</div>
-					<div>
-						<label htmlFor="Message">Message:</label>
-						<br></br>
-						<textarea
-							name="Message"
-							defaultValue={message}
-							onBlur={handleBlank}
-							rows="5"
-						/>
-					</div>
-					{errorMessage && (
-						<div>
-							<p className="error-text">{errorMessage}</p>
-						</div>
-					)}
-					<button type="submit">Submit</button>
-				</form>
-			</div>
-		</section>
-	);
+  return (
+    <section>
+      <h1 data-testid="h1tag">Contact me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button data-testid="button" type="submit">Submit</button>
+      </form>
+    </section>
+  );
 }
 
-export default Contact;
+export default ContactForm;
