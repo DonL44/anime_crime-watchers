12d5759e6d33888b12f365024bea12b2748bcedb
ashleyhodge <ashhodge@comcast.net>
2022-05-18 15:27:27 -0400

    add typedefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,27 @@
-const { User, Post } = require('../models');
+const Post = require("../models/Post");
+const User = require("../models/User");
 
 
+const resolvers = {
+  Query: {
+    // find all users
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+    },
+    // find posts / find all posts by username
+    posts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Post.find(params).sort({ createdAt: -1 });
+    },
+    // find one post by its id
+    post: async (parent, { _id }) => {
+      return Post.findOne({ _id });
+    },
+
+  }
+};
+
+
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,14 +1,42 @@
+// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
+//create typeDefs
 const typeDefs = gql`
   type User {
     _id: ID
     username: String
     email: String
+    description: String
     friendCount: Int
     posts: [Post]
     friends: [User]
   }
+
+  type Post {
+    _id: ID
+    postTitle: String
+    postText: String
+    createdAt: String
+    username: String
+    commentCount: Int
+    comments: [Comment]
+  }
+
+  type Comment {
+    _id: ID
+    commentText: String
+    createdAt: String
+    username: String
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    posts(username: String): [Post]
+    post(_id: ID): Post
+  }
 `;
 
+// export typeDefs
 module.exports = typeDefs;
