14bdbbb370b83bc143368be880a7ee48a656719f
Paul Shertzinger <pshertzi@gmail.com>
2022-05-19 15:08:37 -0400

    basic page layout

---
+++ client/src/components/footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy;Group Project 2022 by Ashley Hodge, Donovan Looney, Tanya Gonzalez, and Paul Shertzinger.</div>
+    </footer>
+  );
+};
+
+export default Footer;
