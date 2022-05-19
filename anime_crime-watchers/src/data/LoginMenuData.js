2571b121b982437579eebaeaea11a05df677b7d3
tanyaleepr <tanyaleepr@gmail.com>
2022-05-19 13:01:36 -0500

    modified points, minamoto, team details, and added new data for the login and link functionality

---
+++ anime_crime-watchers/src/data/LoginMenuData.js
@@ -0,0 +1,26 @@
+const LoginData = [
+    
+        {
+            id:1,
+            url:'/',
+            name:'Home',
+           
+        },
+        {
+            id:2,
+            url:'/courses',
+            name:'Courses'
+        },
+        {
+            id:3,
+            url:'/my-courses',
+            name:'My Courses'
+        },
+        {
+            id:4,
+            url:'/my-profile',
+            name:'My Profile'
+        },
+]
+
+export default LoginData;
