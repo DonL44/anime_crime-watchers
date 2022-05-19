f88c294d5894d4a384cd6f8cd121a8961030d6f7
tanyaleepr <tanyaleepr@gmail.com>
2022-05-19 13:24:23 -0500

    modifying locations in files/folders for better clean code

---
+++ anime_crime-watchers/data/jsonData.js
@@ -0,0 +1,25 @@
+const courses = [
+  {
+    "id": "1",
+    "image": "",
+    "category": "",
+    "title": "",
+    
+    "price": "",
+    "date": "",
+   
+    "img_bg": "",
+  },
+  {
+    "id": "2",
+    "image": "",
+    "category": "",
+    "title": "",
+    
+    "price": "",
+    "date": "",
+   
+    "img_bg": "",
+  }
+]
+export default courses;
