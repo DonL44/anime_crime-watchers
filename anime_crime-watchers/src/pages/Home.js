7aa74728d673d0de81b3e9bf0a5f721036b74e5d
tanyaleepr <tanyaleepr@gmail.com>
2022-05-20 20:10:17 -0500

    fixed connection for Home

--- anime_crime-watchers/src/pages/Home.js
+++ anime_crime-watchers/src/pages/Home.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import About from './About';
+import Cards from '../components/Cards';
+import Analytics from '../components/Analytics';
+import Contact from '../components/Contact';
+
 const Home = () => {
     return (
-      <section className='sectionn text-white'>
-        <h1>home page</h1>
-      </section>
+      <div>
+      <Hero />
+        <About />     
+        <Cards /> 
+        <Analytics />
+        <Contact />
       
+      </div>
     );
   };
   export default Home;
