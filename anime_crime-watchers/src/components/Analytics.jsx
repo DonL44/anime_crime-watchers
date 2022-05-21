dfdc1489aa7a5c0b86b9c929b4926641657c171b
Donovan <Donovan1478@yahoo.com>
2022-05-21 09:40:24 -0500

    added another image

--- anime_crime-watchers/src/components/Analytics.jsx
+++ anime_crime-watchers/src/components/Analytics.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Backdrop from '../assets/images/backdrop.gif'
 // import Laptop from '../assets/laptop.jpg';
 
 const Analytics = () => {
   return (
     <div className='w-full bg-white py-16 px-4'>
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-        {/* <img className='w-[500px] mx-auto my-4' src={Laptop} alt='/' /> */}
+        <img className='w-[500px] h-80 mx-auto my-4' src={Backdrop} alt='/' />
         <div className='flex flex-col justify-center'>
           <p className='text-[#dfc100] font-bold '>Crime Watch Available</p>
           <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Search this map for past and current crime reports.</h1>
@@ -17,6 +18,11 @@ const Analytics = () => {
           </p>
           <button className='bg-black text-[#dfc100] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>Get Started</button>
           {/* Add map, api and submit crime tip links */}
+          {/* <img
+                    className="h-8 w-auto sm:h-80"
+                    src={Backdrop}
+                    alt="Logo"
+                  /> */}
         </div>
       </div>
     </div>
