a8bf50566cc8c43fcb1dda94d956ff67261384af
tanyaleepr <tanyaleepr@gmail.com>
2022-05-24 19:49:21 -0500

    finished style for signup/login and it's well connected

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -9,11 +9,7 @@ function Dashboard(props) {
 			<div className="center">
 				<Card.Body>
 				
-					<image
-					src={require(`../../assets/images/${props.users.imgage}`)}
-					alt="user's profile picture"
-					className="photo"
-				/>
+				
 					<Card.Title className="card-title">{props.users.name}</Card.Title>
 					<Card.Text className="card-text">{props.users.description}</Card.Text>
 					<Card.Subtitle className="card-subtitle">{props.users.technologies} </Card.Subtitle>
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -55,7 +55,7 @@ const ADD = () => {
 
   return (
     <>
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit} className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
         <Alert
           dismissible
           onClose={() => setShowAlert(false)}
@@ -64,14 +64,15 @@ const ADD = () => {
         >
           Something went wrong with your SignUp credentials!
         </Alert>
-        <Form.Group>
-          <Form.Label htmlFor="email">Email</Form.Label>
+        <Form.Group className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+          <Form.Label htmlFor="email" className="mb-8 text-3xl text-center">Email</Form.Label>
           <Form.Control
             type="text"
             placeholder="Your email"
             name="email"
             onChange={handleInputChange}
             value={userFormData.email}
+            className="block border border-grey-light w-full p-3 rounded mb-4"
             required
           />
           <Form.Control.Feedback type="invalid">
@@ -79,7 +80,7 @@ const ADD = () => {
           </Form.Control.Feedback>
         </Form.Group>
 
-        <Form.Group>
+        <Form.Group className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
           <Form.Label htmlFor="password">Password</Form.Label>
           <Form.Control
             type="password"
@@ -87,9 +88,10 @@ const ADD = () => {
             name="password"
             onChange={handleInputChange}
             value={userFormData.password}
+            className="block border border-grey-light w-full p-3 rounded mb-4"
             required
           />
-          <Form.Control.Feedback type="invalid">
+          <Form.Control.Feedback type="invalid" >
             Password is required!
           </Form.Control.Feedback>
         </Form.Group>
@@ -97,6 +99,7 @@ const ADD = () => {
           disabled={!(userFormData.email && userFormData.password)}
           type="submit"
           variant="success"
+          className="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded"
         >
           Submit
         </Button>
